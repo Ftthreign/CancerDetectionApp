87bdcaf37d91a64de78fe1f783c7392c4834e13e
Ftthreign <theownerkill432@gmail.com>
2024-11-02 00:23:26 +0700

    chore: prepare views

--- app/src/main/java/com/dicoding/asclepius/data/AppRepository.kt
+++ app/src/main/java/com/dicoding/asclepius/data/AppRepository.kt
@@ -1,4 +1,25 @@
 package com.dicoding.asclepius.data
 
-class AppRepository {
+import com.dicoding.asclepius.data.local.room.CancerHistoryDao
+import com.dicoding.asclepius.data.remote.retrofit.ApiService
+
+class AppRepository(
+    private val apiService: ApiService,
+    private val cancerHistoryDao: CancerHistoryDao
+) {
+
+    companion object {
+        @Volatile
+        private var instance : AppRepository? = null
+        fun getInstance(
+            apiService: ApiService,
+            cancerHistoryDao: CancerHistoryDao
+        ) : AppRepository =
+            instance ?: synchronized(this) {
+                instance ?: AppRepository(
+                    apiService,
+                    cancerHistoryDao
+                )
+            }.also { instance = it }
+    }
 }
